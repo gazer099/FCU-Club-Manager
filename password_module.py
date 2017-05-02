28843637879daed25946df14f5f6822ccddaf34d
gazer099 <a3104123@gmail.com>
2017-05-03 00:31:02 +0800

    Complete part of the sign up function

--- password_module.py
+++ password_module.py
@@ -1,4 +1,5 @@
 import hashlib
+import getpass
 
 
 def get_digest(password):
@@ -7,3 +8,18 @@ def get_digest(password):
 
 def is_password(password, digest):
     return get_digest(password) == digest
+
+
+def set_password():
+    password = getpass.getpass('Please input your Password\n>>> (Cannot see)')
+    if len(password) < 8 or password == '':
+        print('<<Your password format is incorrect, Please try again>>')
+        return set_password()
+
+    password_again = getpass.getpass('Please input your Password AGAIN\n>>> (Cannot see)')
+
+    if password != password_again:
+        print('<<Your password entry is inconsistent, Please try again>>')
+        return set_password()
+    else:
+        return get_digest(password.encode())
