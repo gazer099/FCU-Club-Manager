fb2bbc431f16e60ba2d5d59cde5a1f3358abbc77
gazer099 <a3104123@gmail.com>
2017-05-28 19:43:36 +0800

    Object-oriented RoadSection

--- RoadSection.py
+++ RoadSection.py
@@ -3,34 +3,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 import date_dictionary
 
-file_name = '01F2483N-03F2709S.csv'
-dict_entire = date_dictionary.get_dict_entire()
-flow_all = []
 
+class RoadSection:
+    file_name = None
+    dict_entire = date_dictionary.get_dict_entire()
+    flow_all = []
 
-def fix_header():
-    with open('Fixed_01F2483N-03F2709S.csv', 'w', newline='') as fixed_file:
-        fixed = csv.writer(fixed_file)
-        fixed.writerow(['no','start','end','type31','travel31','flow31','type32','travel32','flow32','type41','travel41','flow41','type42','travel42','flow42','type5','travel5','flow5','avetime','date','time'])
-        with open('01F2483N-03F2709S.csv') as original_file:
-            ori = csv.reader(original_file)
-            for row in ori:
-                if ori.line_num == 1:
-                    continue
-                fixed.writerow(row)
+    def __init__(self, file_name):
+        self.file_name = file_name
 
-fix_header()
+    def load(self):
+        self.fix_header()
+        with open('Fixed_'+self.file_name) as file:
+            for row in csv.DictReader(file):
+                if row['date'] in self.dict_entire:
+                    self.dict_entire[row['date']] += int(row['flow31']) + int(row['flow32']) + int(row['flow41']) + int(
+                        row['flow42']) + int(row['flow5'])
 
-with open('Fixed_01F2483N-03F2709S.csv') as file:
-    for row in csv.DictReader(file):
-        if row['date'] in dict_entire:
-            dict_entire[row['date']] += int(row['flow31'])+int(row['flow32'])+int(row['flow41'])+int(row['flow42'])+int(row['flow5'])
+        for row in dict.items(self.dict_entire):
+            print(row, type(row))
+            self.flow_all.append(row[1])
+        print(self.flow_all)
 
-for row in dict.items(dict_entire):
-    print(row, type(row))
-    flow_all.append(row[1])
-print(flow_all)
+    def show(self):
+        self.flow_all = np.array(self.flow_all)
+        plt.plot(self.flow_all)
+        plt.show()
 
-flow_all = np.array(flow_all)
-plt.plot(flow_all)
-plt.show()
+    def fix_header(self):
+        with open('Fixed_' + self.file_name, 'w', newline='') as fixed_file:
+            fixed = csv.writer(fixed_file)
+            fixed.writerow(
+                ['no', 'start', 'end', 'type31', 'travel31', 'flow31', 'type32', 'travel32', 'flow32', 'type41',
+                 'travel41', 'flow41', 'type42', 'travel42', 'flow42', 'type5', 'travel5', 'flow5', 'avetime', 'date',
+                 'time'])
+            with open(self.file_name) as original_file:
+                ori = csv.reader(original_file)
+                for row in ori:
+                    if ori.line_num == 1:
+                        continue
+                    fixed.writerow(row)
+
+if __name__ == '__main__':
+    rs = RoadSection('01F2483N-03F2709S.csv')
+    rs.load()
+    rs.show()
