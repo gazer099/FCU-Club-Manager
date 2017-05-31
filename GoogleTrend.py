2e05e741f581f649310f3c909186a891141d6433
gazer099 <a3104123@gmail.com>
2017-05-31 18:12:59 +0800

    Solve UnicodeDecodeError

--- GoogleTrend.py
+++ GoogleTrend.py
@@ -18,30 +18,26 @@ class GoogleTrend:
         self.file_name = file_name
 
     def load(self):
-        with open(self.file_name) as file:
-            line_list = []
-            for line in file:
-                line_list.append(line)
-            print(line_list)
-            # for idx, line in enumerate(line_list):
-            #     if idx == 0:
-            #         self.category = line[3:]
-            #     if idx == 2:
-            #         # todo solve split problem
-            #         self.target = line[2:]
-            #     if idx >= 3 and line != '':
-            #         line = line.split(',')
-            #         line[0] = line[0].replace('-', '/')
-            #         self.dict_entire[line[0]] = line[1]
-            #         if self.trend_start_day is None:
-            #             self.trend_start_day = line[0]
-            #         else:
-            #             self.trend_end_day = line[0]
+        with open(self.file_name, encoding='utf8') as file:
+            for idx, line in enumerate(file):
+                if idx == 0:
+                    self.category = line[3:-1]
+                if idx == 2:
+                    # todo solve split problem
+                    self.target = line[2:-1]
+                if idx >= 3 and line != '':
+                    line = line.split(',')
+                    line[0] = line[0].replace('-', '/')
+                    self.dict_entire[line[0]] = line[1]
+                    if self.trend_start_day is None:
+                        self.trend_start_day = line[0]
+                    else:
+                        self.trend_end_day = line[0]
 
     def show_info(self):
         print('## Road Section Info.')
         print('File name:', self.file_name)
-        print('Category', self.category)
+        print('Category:', self.category)
         print('Target:', self.target)
         print('Trend start day:', self.trend_start_day)
         print('Trend end day:', self.trend_end_day)
@@ -50,6 +46,6 @@ class GoogleTrend:
 if __name__ == '__main__':
     gt = GoogleTrend('multiTimeline.csv')
     gt.load()
-    # gt.show_info()
+    gt.show_info()
     # for i in gt.dict_entire:
     #     print(i)
