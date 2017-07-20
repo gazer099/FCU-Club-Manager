ea9c33ae83a9661e9c29f5455641b6e9d254d549
gazer099 <a3104123@gmail.com>
2017-07-20 21:20:10 +0800

    Group the data according to the week - df_flow_week - df_trend_week

--- PandasType.py
+++ PandasType.py
@@ -15,9 +15,10 @@ rs.normalize()
 
 print(len(gt.trend_percentage))
 print(len(rs.flow_all))
-print(rs.really_start_day_index)
+print(rs.really_start_day_index)  # 416
+print(gt.trend_start_day_index)
 
-dates = pd.date_range('20150101', periods=639)
+# dates = pd.date_range('20150101', periods=639)
 df = pd.DataFrame({
     'trend': gt.trend_percentage,
     'flow': rs.flow_all
@@ -25,7 +26,30 @@ df = pd.DataFrame({
     # index=dates
 )
 
-print(df)
+flow_week_dict = {
+    '0': rs.flow_all[rs.really_start_day_index::7],
+    '1': rs.flow_all[rs.really_start_day_index + 1::7],
+    '2': rs.flow_all[rs.really_start_day_index + 2::7],
+    '3': rs.flow_all[rs.really_start_day_index + 3::7],
+    '4': rs.flow_all[rs.really_start_day_index + 4::7],
+    '5': rs.flow_all[rs.really_start_day_index + 5::7],
+    '6': rs.flow_all[rs.really_start_day_index + 6::7]
+}
+trend_week_dict = {
+    '0': gt.trend_percentage[rs.really_start_day_index::7],
+    '1': gt.trend_percentage[rs.really_start_day_index + 1::7],
+    '2': gt.trend_percentage[rs.really_start_day_index + 2::7],
+    '3': gt.trend_percentage[rs.really_start_day_index + 3::7],
+    '4': gt.trend_percentage[rs.really_start_day_index + 4::7],
+    '5': gt.trend_percentage[rs.really_start_day_index + 5::7],
+    '6': gt.trend_percentage[rs.really_start_day_index + 6::7]
+}
+df_flow_week = pd.DataFrame.from_dict(flow_week_dict, orient='index').T
+df_trend_week = pd.DataFrame.from_dict(trend_week_dict, orient='index').T
+
+print(df_flow_week)
+print(df_trend_week)
+exit()
 
 point = rs.really_start_day_index + 10  # 先跳過有問題那幾筆
 cases = []
@@ -43,7 +67,7 @@ for j in range(0, 100):
     cases_test.append(five_days)
     labels_test.append([df.iloc[point + 5, 0]])
     point += 1
-
+exit()
 nn = bpnn.BPNeuralNetwork()
 nn.setup(10, 22, 1)
 nn.train(cases, labels)
