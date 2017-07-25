fb1249f343d26ed335ed723b65d8dd0e0397de56
gazer099 <a3104123@gmail.com>
2017-07-25 22:02:09 +0800

    Complete part of type 2

--- PandasType.py
+++ PandasType.py
@@ -5,25 +5,35 @@ import GoogleTrend
 import RoadSection
 import bpnn
 
-# Load Data-----------------------------------------------------
-gt = GoogleTrend.GoogleTrend('multiTimeline.csv')
-gt.load()
-gt.normalize()
 
+def normalize(series, x):
+    return 2 / (series.max() - series.min()) * (x - series.min()) - 1
+
+
+def normalize_all(series):
+    series_max = series.max()
+    series_min = series.min()
+    return pd.Series([(lambda x: 2 / (series_max - series_min) * (x - series_min) - 1)(x) for x in series])
+
+
+# Load Data-----------------------------------------------------
 rs = RoadSection.RoadSection('03F2614S-03F2709S.csv')
 rs.load()
-rs.normalize()
+# rs.normalize()
 
-print(len(gt.trend_percentage))
+gt = GoogleTrend.GoogleTrend('multiTimeline.csv')
+gt.load()
+# gt.normalize()
+
 print(len(rs.flow_all))
-print(rs.really_start_day_index)  # 416
-print(gt.trend_start_day_index)
+print(len(gt.trend_percentage))
+assert rs.really_start_day_index == gt.trend_start_day_index  # 416
 
 # Store to Pandas DataFrame Type-------------------------------------------
 # dates = pd.date_range('20150101', periods=639)
 df = pd.DataFrame({
-    'trend': gt.trend_percentage,
-    'flow': rs.flow_all
+    'trend': gt.trend_percentage,  # df[:, 1]
+    'flow': rs.flow_all  # df[:, 0]
 },
     # index=dates
 )
@@ -48,29 +58,86 @@ trend_week_dict = {
 }
 df_flow_week = pd.DataFrame.from_dict(flow_week_dict, orient='index').T
 df_trend_week = pd.DataFrame.from_dict(trend_week_dict, orient='index').T
-
+print(df)
 print(df_flow_week)
 print(df_trend_week)
 # exit()
 
 # Type 1 Neural Network---------------------------------------------------------------
-point = rs.really_start_day_index + 10  # 先跳過有問題那幾筆
+# point = rs.really_start_day_index + 10  # 先跳過有問題那幾筆
+# cases = []
+# labels = []
+# for i in range(0, 100):
+#     five_days = list(df.iloc[point:point + 5, 0]) + list(df.iloc[point:point + 5, 1])
+#     cases.append(five_days)
+#     labels.append([df.iloc[point + 5, 0]])
+#     point += 1
+#
+# cases_test = []
+# labels_test = []
+# for j in range(0, 100):
+#     five_days = list(df.iloc[point:point + 5, 0]) + list(df.iloc[point:point + 5, 1])
+#     cases_test.append(five_days)
+#     labels_test.append([df.iloc[point + 5, 0]])
+#     point += 1
+# exit()
+# nn = bpnn.BPNeuralNetwork()
+# nn.setup(10, 2, 1)
+# nn.train(cases, labels)
+# predict_all = nn.test(cases_test, labels_test)
+
+# plt.plot(labels_test, 'b')
+# plt.plot(predict_all, 'r')
+# plt.show()
+
+# Type 2 Neural Network---------------------------------------------------------------
+df_flow_week_sub_mean = df_flow_week.copy()
+df_trend_week_sub_mean = df_trend_week.copy()
+assert df_flow_week_sub_mean.shape == df_trend_week_sub_mean.shape
+
+for col in range(df_flow_week_sub_mean.shape[1]):
+    df_flow_week_sub_mean.iloc[:, col] -= df_flow_week_sub_mean.iloc[:, col].mean()
+    df_trend_week_sub_mean.iloc[:, col] -= df_trend_week_sub_mean.iloc[:, col].mean()
+print(df_flow_week_sub_mean)
+print(df_trend_week_sub_mean)
+
+series_flow_week_sub_mean = pd.Series(pd.concat(
+    [(lambda row: df_flow_week_sub_mean.iloc[row, :])(row) for row in range(df_flow_week_sub_mean.shape[0])], axis=0,
+    ignore_index=True))
+print(series_flow_week_sub_mean)
+series_trend_week_sub_mean = pd.concat(
+    [(lambda row: df_trend_week_sub_mean.iloc[row, :])(row) for row in range(df_trend_week_sub_mean.shape[0])], axis=0,
+    ignore_index=True)
+print(series_trend_week_sub_mean)
+
+point = 10  # 先跳過有問題那幾筆
 cases = []
 labels = []
+series_flow_week_sub_mean_normalization = normalize_all(series_flow_week_sub_mean[point:point + 100 + 5])
+print('***', len(series_flow_week_sub_mean_normalization), point)
+series_trend_week_sub_mean_normalization = normalize_all(series_flow_week_sub_mean[point:point + 100 + 5])
 for i in range(0, 100):
-    five_days = list(df.iloc[point:point + 5, 0]) + list(df.iloc[point:point + 5, 1])
+    five_days = list(series_flow_week_sub_mean_normalization[i:i + 5]) + list(
+        series_trend_week_sub_mean_normalization[i:i + 5])
     cases.append(five_days)
-    labels.append([df.iloc[point + 5, 0]])
+    print(i, i + 5, [series_flow_week_sub_mean_normalization[i + 5]])
+    labels.append([series_flow_week_sub_mean_normalization[i + 5]])
     point += 1
 
 cases_test = []
 labels_test = []
-for j in range(0, 100):
-    five_days = list(df.iloc[point:point + 5, 0]) + list(df.iloc[point:point + 5, 1])
+series_flow_week_sub_mean_normalization = normalize_all(series_flow_week_sub_mean[point:point + 100 + 5])
+series_trend_week_sub_mean_normalization = normalize_all(series_flow_week_sub_mean[point:point + 100 + 5])
+for i in range(0, 100):
+    five_days = list(series_flow_week_sub_mean_normalization[i:i + 5]) + list(
+        series_trend_week_sub_mean_normalization[i:i + 5])
     cases_test.append(five_days)
-    labels_test.append([df.iloc[point + 5, 0]])
+    print(i, i + 5, [series_flow_week_sub_mean_normalization[i + 5]])
+    labels_test.append([series_flow_week_sub_mean_normalization[i + 5]])
     point += 1
-# exit()
+for i in cases_test:
+    print(i)
+exit()
 nn = bpnn.BPNeuralNetwork()
 nn.setup(10, 2, 1)
 nn.train(cases, labels)
@@ -79,3 +146,5 @@ predict_all = nn.test(cases_test, labels_test)
 plt.plot(labels_test, 'b')
 plt.plot(predict_all, 'r')
 plt.show()
+
+print(predict_all)
