fe1b689dd992b0e1f0324ff10b490cdd87abbcba
gazer099 <a3104123@gmail.com>
2017-09-03 17:58:17 +0800

    Add new trends for correlation

--- correlation.py
+++ correlation.py
@@ -10,7 +10,7 @@ import os
 import scipy as sp
 
 # Load Data-----------------------------------------------------
-gt = GoogleTrend.GoogleTrend('雲林.csv')
+gt = GoogleTrend.GoogleTrend('multiTimeline.csv')
 gt.load()
 gt.normalize()
 
@@ -22,18 +22,52 @@ print(len(rs.flow_all))
 print(len(gt.trend_percentage))
 assert rs.really_start_day_index == gt.trend_start_day_index  # 416
 
+gt2_trend_percentage = []
+with open('GoogleTrendData/1_北港朝天宮.csv', 'r', encoding='utf8') as file:
+    for line in file:
+        if line[0:4] == '2016':
+            data = line.split(',')
+            gt2_trend_percentage.append(int(data[1]) / 100)
+            # print(int(data[1]))
+print(len(gt2_trend_percentage))
+
+gt3_trend_percentage = []
+with open('GoogleTrendData/7_雲林故事館.csv', 'r', encoding='utf8') as file:
+    for line in file:
+        if line[0:4] == '2016':
+            data = line.split(',')
+            gt3_trend_percentage.append(int(data[1]) / 100)
+            # print(int(data[1]))
+print(len(gt3_trend_percentage))
+
+t1 = []
+t2 = []
+t3 = []
+with open('GoogleTrendData/top3.csv', 'r', encoding='utf8') as file:
+    for line in file:
+        if line[0:4] == '2016':
+            data = line.split(',')
+            t1.append(int(data[1]) / 100)
+            t2.append(int(data[2]) / 100)
+            t3.append(int(data[3]) / 100)
+# exit()
 # Store to Pandas DataFrame Type-------------------------------------------
 # dates = pd.date_range('20150101', periods=639)
 df = pd.DataFrame({
-    'flow': rs.flow_all,
-    'trend': gt.trend_percentage
+    '0flow': rs.flow_all[416:],
+    # 'trend': gt.trend_percentage[416:],
+    # 'trend2': gt2_trend_percentage,
+    # 'trend3': gt3_trend_percentage
+    '1': t1,
+    '2': t2,
+    '3': t3,
 },
     # index=dates
 )
 
 print(df)
-# plt.plot(df[416:])
-# plt.show()
+plt.plot(df[:125])
+plt.show()
 print(sp.stats.pearsonr(rs.flow_all[416:], gt.trend_percentage[416:]))
 # del gt
 # gt_temple = GoogleTrend.GoogleTrend('北港朝天宮.csv')
